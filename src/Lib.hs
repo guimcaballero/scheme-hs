9476c89d4aeb9b07df8a2a71bfe9922f3f7dc3b7
guimcaballero <guim.caballero@gmail.com>
2021-01-01 23:00:13 +0100

    Add REPL

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,27 +3,54 @@ module Lib
     ) where
 
 import System.Environment
+import System.IO
+import Control.Monad.Except
 import Text.ParserCombinators.Parsec hiding (spaces)
 
 import Parsing
 import Types
 import Primitives
 
-import Control.Monad.Except
-
 main :: IO ()
 main = do
-     args <- getArgs
-     let evaled = fmap show $ readExpr (head args) >>= eval
-     putStrLn $ extractValue $ trapError evaled
+  args <- getArgs
+  case length args of
+    0 -> runRepl
+    1 -> evalAndPrint $ head args
+    _ -> putStrLn "Program takes only 0 or 1 argument"
 
 readExpr :: String -> ThrowsError LispVal
 readExpr input = case parse parseExpr "lisp" input of
      Left err -> throwError $ Parser err
      Right val -> return val
 
+trapError :: ThrowsError String -> ThrowsError String
 trapError action = catchError action (return . show)
 
 extractValue :: ThrowsError a -> a
 extractValue (Right val) = val
 extractValue _ = error "Extracting from a Left"
+
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+evalString :: String -> String
+evalString expr = extractValue $ trapError (show <$> (readExpr expr >>= eval))
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr = putStrLn $ evalString expr
+
+until_ :: Monad m => (String -> Bool) -> m String -> (String -> m ()) -> m ()
+until_ endPred prompt action = do
+  result <- prompt
+  if null result
+    then until_ endPred prompt action
+    else if endPred result
+           then return ()
+           else action result >> until_ endPred prompt action
+
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt "Scheme>=> ") evalAndPrint
